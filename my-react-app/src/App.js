befabeb6cc7306a03bf0192bbc878fa7cebb503b
unknown <visheshj14@gmail.com>
2024-12-10 13:20:20 +0530

    first

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import First from './component/First';
+import Nav from './component/First';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <>
+    <Counter></Counter>
+    {/* <Nav/>
+    <First>
+      hello there
+    </First> */}
+    </>
+  );
+}
+export default App;
---
+++ my-react-app/src/component/Car.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Car extends React.Component {
+    render(){
+        return(
+            <>
+            <h1>Car</h1>
+            </>
+        )
+    }
+}
